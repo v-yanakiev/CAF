c63dd7828783f033c74de3ac54620c79757a244b
Vasil Yanakiev <vaskoleon@gmail.com>
2018-08-30 06:38:43 +0300

    minorChangesToModels

--- ChoresAndFulfillment.Models/Job.cs
+++ ChoresAndFulfillment.Models/Job.cs
@@ -1,5 +1,6 @@
 ﻿using ChoresAndFulfillment.Models.Enums;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChoresAndFulfillment.Models
 {
@@ -12,10 +13,13 @@ namespace ChoresAndFulfillment.Models
             this.WorkerConfirmedFinished = false;
         }
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
         public int JobCreatorId { get; set; }
         public EmployerAccount JobCreator { get; set; }
+        [Required]
         public decimal PayUponCompletion { get; set; }
         public List<WorkerAccountApplication> Applicants { get; set; }
         public int? HiredUserId { get; set; }
